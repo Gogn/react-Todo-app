63f735558c12c2786f2d825318dcd362e2201162
Dero <106393@gmail.com>
2020-01-31 14:53:10 +0400

    Updates

---
+++ precache-manifest.f899c7c8af2f5e534b02ed22c2550c77.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "014821ed410257898c340f1656017a9a",
+    "url": "/react-Todo-app/index.html"
+  },
+  {
+    "revision": "fbe8c29dcd7477cf11af",
+    "url": "/react-Todo-app/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "c35e7e26006c9d70bed5",
+    "url": "/react-Todo-app/static/css/main.23ebafaf.chunk.css"
+  },
+  {
+    "revision": "fbe8c29dcd7477cf11af",
+    "url": "/react-Todo-app/static/js/2.2f62dc42.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/react-Todo-app/static/js/2.2f62dc42.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c35e7e26006c9d70bed5",
+    "url": "/react-Todo-app/static/js/main.78b46b53.chunk.js"
+  },
+  {
+    "revision": "c870a6482d3fea322c6a",
+    "url": "/react-Todo-app/static/js/runtime-main.d5b03542.js"
+  }
+]);
