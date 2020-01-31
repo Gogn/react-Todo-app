6c5a0144f7f0162eec8cfbe6aca9da5c9f7572a3
Dero <106393@gmail.com>
2020-01-31 15:01:37 +0400

    Fix

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,9 +2,9 @@ import React from 'react'
 import {NavLink} from "react-router-dom";
 
 export const Navbar = () => (
-  <nav className='navbar navbar-dark bg-primary navbar-expand-lg'>
+  <nav className='navbar navbar-dark bg-primary navbar-expand'>
     <div className="navbar-brand">
-      <img src='public/favicon.ico' width="30" alt="React app" className='d-inline-block align-top mr-3' />
+      <img src='favicon.ico' width="30" alt="React app" className='d-inline-block align-top mr-3' />
       Todo app. Local state version.
     </div>
     <ul className='navbar-nav ml-auto'>
